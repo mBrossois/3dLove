52ad358bf5464b415403911229e9715ddd6365ee
mark de Neut Brossois <mark.dnb@live.nl>
2022-04-30 22:16:42 +0200

    Copy change

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'I love you Emma',
             {
                 font: font,
                 size: 0.5,
